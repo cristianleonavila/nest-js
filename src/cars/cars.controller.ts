adca42c42dce66236ca415f9302ae2de12cf6836
cleonavila <cleon@almagrario.com>
2026-04-04 23:18:58 -0500

    Ajustes

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CarsService } from './cars.service';
+import { UpdateCarDTO, CreateCarDTO } from './dto';
 
 @Controller('cars')
 export class CarsController {
@@ -16,7 +17,28 @@ export class CarsController {
     }
 
     @Get(':id')
-    getCarById( @Param('id') id ) {
+    getCarById( @Param('id', ParseUUIDPipe) id:string ) {
         return this.carsService.findById(id);
     }
+
+     @Post() 
+     create(@Body() body: CreateCarDTO) {
+        return this.carsService.create(body);
+     }
+
+     @Patch(':id')
+     updateCar(
+        @Param('id', ParseUUIDPipe) id, 
+        @Body() updateDTO: UpdateCarDTO) {
+
+        return this.carsService.update(id, updateDTO);
+     }
+
+     @Delete(':id')
+     deleteCar(@Param('id', ParseUUIDPipe) id: string) {
+        return {
+            id,
+            method: 'DELETE'
+        }
+     }
 }
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,21 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Car } from './interfaces/car.interface';
+import { v4 as uuid } from 'uuid';
+import { CreateCarDTO, UpdateCarDTO } from './dto';
+
 
 @Injectable()
 export class CarsService {
 
-    private data =  [
+    private data:Car[] =  [
         {
-            id: 1,
+            id: uuid(),
             brand: 'Toyota',
             model: 'A'
         },
         {
-            id: 2,
+            id: uuid(),
             brand: 'Chevrolet',
             model: 'B'
         },
         {
-            id: 3,
+            id: uuid(),
             brand: 'Honda',
             model: 'C'
         }
@@ -25,8 +29,37 @@ export class CarsService {
         return this.data;
     }
 
-    findById( id: number) {
-        return this.data.find((car) => car.id == id) ?? {};
+    findById( id: string) {
+        const car = this.data.find((car) => car.id == id);
+        if ( !car ) {
+            throw new NotFoundException(`Car id: ${id} not found`);
+        }
+        return car;
+    }
+
+    create(carDTO: CreateCarDTO) {
+        const car: Car = {
+            id: uuid(),
+            ...carDTO
+        };
+        this.data.push(car);
+        return car;
+    }
+
+    update(id: string, updateDTO: UpdateCarDTO) {
+        let oldCar = this.findById(id);
+        this.data = this.data.map(car => {
+            if ( car.id === id ) {
+                oldCar = {
+                    ...oldCar,
+                    ...updateDTO,
+                    id
+                };
+                return oldCar;
+            }
+            return car;
+        });
+        return oldCar;
     }
 
 }
